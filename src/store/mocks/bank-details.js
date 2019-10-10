2738f45b11e8b229ccc2995ac488bca99e1b2505
shivabetel <shiva.betel@gmail.com>
2019-10-10 08:26:12 +0000

    Update bank-details.js

---
+++ src/store/mocks/bank-details.js
@@ -0,0 +1,158 @@
+export const bankList = {
+  'bankList': {
+    data: { 'banks': [
+      {
+        'bankId': '2342343',
+        'bankName': 'HDFC',
+        'bankType': 'abcd'
+      },
+      {
+        'bankId': '3543554',
+        'bankName': 'AXIS',
+        'bankType': 'pqr'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      },
+      {
+        'bankId': '345345',
+        'bankName': 'SBI',
+        'bankType': 'xyz'
+      }
+    ] },
+    apiResponseCode: 'success'
+  }
+
+}
+
+export const branchByIfsc = {
+  'branchByIfsc': {
+    data: { 'bank': {
+      'bankId': '345345',
+      'bankName': 'SBI',
+      'bankType': 'xyz'
+    },
+    'branch': {
+      'branchId': '4564565',
+      'branchName': 'Thane',
+      'ifscCode': 'SBI000435'
+    },
+    'city': {
+      'cityId': '34334',
+      'cityName': 'Thane'
+    } },
+    apiResponseCode: 'success'
+  }
+}
+
+export const bankBranchList = {
+  'bankBranchList': {
+    data: { 'branches': [
+      {
+        'branchId': '234324',
+        'branchName': 'Thane',
+        'ifscCode': 'SBI000343'
+      },
+      {
+        'branchId': '3453454',
+        'branchName': 'Vashi',
+        'ifscCode': 'SBI0004545'
+      },
+      {
+        'branchId': '32434',
+        'branchName': 'Mulund',
+        'ifscCode': 'SBI0090490'
+      }
+    ] },
+    apiResponseCode: 'success'
+  }
+}
+
+export const bankBranchWithIfsc = {
+  'bankBranchWithIfsc': {
+    data: { 'branches': [
+      {
+        'branchId': '234324',
+        'branchName': 'Thane',
+        'ifscCode': 'SBI000343'
+      },
+      {
+        'branchId': '3453454',
+        'branchName': 'Vashi',
+        'ifscCode': 'SBI0004545'
+      },
+      {
+        'branchId': '32434',
+        'branchName': 'Mulund',
+        'ifscCode': 'SBI0090490'
+      }
+    ] },
+    apiResponseCode: 'success'
+  }
+}
+
+export const bankCityList = {
+  'bankCityList': {
+    data: { 'cities': [
+      {
+        'cityId': '3434',
+        'cityName': 'Thane'
+      },
+      {
+        'cityId': '3444',
+        'cityName': 'Mulund'
+      },
+      {
+        'cityId': '32434',
+        'cityName': 'Dombivili'
+      }
+    ] },
+    apiResponseCode: 'success'
+  }
+}
+
+export const bankIfscCode = {
+  'bankIfscCode': {
+    data: {
+      'ifscCode': 'SBI0090490'
+    },
+    apiResponseCode: 'success'
+  }
+}
